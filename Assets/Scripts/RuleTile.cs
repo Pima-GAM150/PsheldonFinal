52789aa88ecf6a0ab47f27edafb17a3e4867c4fb
Psheldon <psheldon@mail.pima.edu>
2018-11-15 19:12:25 -0700

    v0.1.0.2. Added rules to tiles, and initial scripting for unit.

---
+++ Assets/Scripts/RuleTile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu]
+public class RuleTile: Tile {
+
+    public string tileType;
+    public bool isPassable;
+    public bool isFlyable;
+    public int moveCost;
+    public int defBonus;
+    public int dodgeBonus;
+	
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Unit {
+
+    public string name;
+    public string job;
+    public int lvl;
+    public int exp;
+    public int[] stats = new int[8];
+    public int[] statGrowths = new int[8];
+
+}
