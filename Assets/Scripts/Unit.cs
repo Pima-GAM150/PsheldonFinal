d02e6242068dae32f0e120c01662a6758d27deca
Psheldon <psheldon@mail.pima.edu>
2018-11-15 19:54:35 -0700

    v0.1.0.2.1, added to unit script.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class Unit {
+public class Unit{
 
     public string name;
     public string job;
@@ -11,5 +10,32 @@ public class Unit {
     public int exp;
     public int[] stats = new int[8];
     public int[] statGrowths = new int[8];
+
+    public Unit()
+    {
+        name = "Sample";
+        job = "Sample";
+        lvl = 1;
+        exp = 0;
+        for (int i = 1; i < stats.Length; i++)
+        {
+            stats[i] = 1;
+        }
+        for (int i = 1; i < statGrowths.Length; i++)
+        {
+            statGrowths[i] = 0;
+        }
+    }
+
+    public Unit(string newName, string newJob, int lvl, 
+        int exp, int[] stats, int[] statGrowths)
+    {
+        name = newName;
+        job = newJob;
+        this.lvl = lvl;
+        this.exp = exp;
+        this.stats = stats;
+        this.statGrowths = statGrowths;
+    }
 
 }
