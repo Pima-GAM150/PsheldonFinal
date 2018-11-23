300e0112df4d1ac8c1a76974a3710b54e27d9537
Psheldon <psheldon@mail.pima.edu>
2018-11-23 10:51:25 -0700

    added auto loading of party units.

---
+++ Assets/Editor/PartyAutoConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class PartyAutoConverter : AssetPostprocessor {
+
+    static Dictionary<string, Action> parsers;
+
+    static PartyAutoConverter()
+    {
+        parsers = new Dictionary<string, Action>();
+        parsers.Add("partydata.csv", ParseParty);
+    }
+
+    static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        for (int i = 0; i < importedAssets.Length; i++)
+        {
+            string fileName = Path.GetFileName(importedAssets[i]);
+            if (parsers.ContainsKey(fileName))
+                parsers[fileName]();
+        }
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+
+    static void ParseParty()
+    {
+        string filePath = Application.dataPath + "/Settings/partydata.csv";
+        if (!File.Exists(filePath))
+        {
+            Debug.LogError("Missing Party Data: " + filePath);
+            return;
+        }
+
+        string[] readText = File.ReadAllLines("Assets/Settings/partydata.csv");
+        filePath = "Assets/Settings/Resources";
+        for (int i = 1; i < readText.Length; i++)
+        {
+            PlayerUnit playerUnit = ScriptableObject.CreateInstance<PlayerUnit>();
+            playerUnit.Load(readText[i]);
+            string fileName = string.Format("{0}{1}.asset", filePath, playerUnit.charName);
+            AssetDatabase.CreateAsset(playerUnit, fileName);
+        }
+    }
+	
+}
