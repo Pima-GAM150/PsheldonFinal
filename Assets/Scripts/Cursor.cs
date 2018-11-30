92d13125ebf8722c4a2a1f8f2b4e5e9b73500aeb
Psheldon <psheldon@mail.pima.edu>
2018-11-29 17:15:15 -0700

    Expanded and fixed cursor script.

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -4,25 +4,37 @@ using UnityEngine;
 
 public class Cursor : MonoBehaviour {
 
+    private GameObject selected = null;
+
 	// Use this for initialization
 	void Start () {
-		
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if ( Input.GetMouseButtonDown(0) ){
-            Debug.Log("Mouse has been clicked.");
-
             RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
             if (hitInfo.collider != null)
             {
-                Debug.Log("Selected " + hitInfo.transform.gameObject.name);
+                if(selected == null || selected.name != hitInfo.transform.gameObject.name)
+                {
+                    selected = hitInfo.transform.gameObject;
+                    Debug.Log("Selected " + selected.name);
+                }
+                
+            } else {
+                if (selected != null)
+                {
+                    Debug.Log("Deselecting " + selected.name);
+                    selected = null;
+                } else
+                {
+                    Debug.Log("Nobody to select.");
+                }
+                
             }
-            else
-            {
-                Debug.Log("No selection.");
-            }
+
         }
 	}
 }
